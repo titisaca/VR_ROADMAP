2ff2f323265a35a0f584d0a5740dce9e71c70975
IMTC505Lab4 <lesliesaca15@gmail.com>
2022-11-25 08:59:18 -0800

    tour icons added to the menu prefabs

--- VR/My project (1)/Assets/Oculus/Interaction/Runtime/Scripts/Input/OVRIntegration/InputDataProviderUpdateTriggerOVR.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:757f21a215454e85fc08f314cb9d0ac2dd48da8c0e5477f0d32079a1632228fa
-size 986
--- VR/My project (1)/Assets/Oculus/Interaction/Runtime/Scripts/Interaction/Transformable/TwoHandFreeTransformer.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:996a2c612290d516f945e5000c6cc79ad131e04c37179e534836da48cf4c4b73
-size 967
